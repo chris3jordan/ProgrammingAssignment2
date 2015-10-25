aef2252ebc50212416471ad2dbc8d199ec2188bf
chris3jordan <chris.jordan@noaa.gov>
2015-10-25 15:35:34 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions that make a globally scoped vector of functions to handle the task of inverting a matrix and
+## caching the inverse.  If the inverse has been generated, it is cached so that future calls won't recalculate the matrix 
+## inverse.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(y = matrix()) ## assume y is an [n x n] invertable matrix
+{
+        
+        ## make two globally scoped matrices of size [y]
+        w <<- y
+        z <<- matrix(nrow = nrow(y), ncol = ncol(y))
+     
+        hasinverse <- function() z[1, 1] ## use check to see if z contains numeric or "NA" values
+   
+        setinverse <- function(a) z <<- a ## cache the inverse of w in z 
+        
+        getinverse <- function() z ## return inverse of w
+        
+        ## function returns a globally scoped list of calls to data and inverse 
+        
+        x <<- list(getinverse = getinverse,
+             hasinverse = hasinverse,
+             setinverse = setinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(y = matrix()) ## assume y is an [n x n] invertable matrix 
+{
+       
+        if(!is.na(x$hasinverse())) ## test if <hasinverse> is numeric, if so use cached data
+        {
+                message("Inverse previously calculated, using cached data")
+                a <- x$getinverse()
+        }
+        
+        else ## if <hasinverse> is "NA", calculate inverse of y, return and cache inverse
+        {
+                message("Inverse not previously calculated, using solve()")
+        		a <- solve(y)
+        		x$setinverse(a)
+        }
+        
+        return(a)
 }
